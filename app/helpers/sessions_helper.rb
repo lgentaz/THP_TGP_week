616f034c541e14a6ad7253d564b8f1b47d812b5e
lgentaz <lucile.gentaz@gmail.com>
2020-05-07 17:57:38 +0200

    dropdown

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+    
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    
+    def signed_in?
+        !current_user.nil?
+    end
+    
 end
