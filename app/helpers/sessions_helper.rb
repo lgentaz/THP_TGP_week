50da25205431268c66b6ee68f53593d007332484
lgentaz <lucile.gentaz@gmail.com>
2020-05-07 19:05:00 +0200

    update your own

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,10 @@ module SessionsHelper
     def current_user
         User.find_by(id: session[:user_id])
     end
+
+    def current_user?(user)
+        user == current_user
+    end
     
     def log_in(user)
         session[:user_id] = user.id
